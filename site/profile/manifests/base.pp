4df92c480c6bb47bc8897e61c9a0df8094fc613e
Justin Zhang <justin.zhang@digitalalchemy.asia>
2017-06-01 18:21:24 +0800

    add sudo

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -4,3 +4,11 @@ class profile::base {
     mode => disabled,
     }
 }
+
+class {'::sudo':} 
+  sudo::conf {'wheel':
+    content => "%wheel ALL=(ALL) NOPASSWD: ALL",
+  }
+  sudo::conf {'rbackup':
+    content => "Defaults:rbackup !requiretty
+rbackup ALL = NOPASSWD: /usr/bin/rsync"}
