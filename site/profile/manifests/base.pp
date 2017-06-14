1238108a8e5f4fb0238ea4e06b1aed0c3874e78e
Justin Zhang <justin.zhang@digitalalchemy.asia>
2017-06-14 18:42:15 +1000

    update sudo setting

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -9,7 +9,7 @@ class {'::sudo':}
     content => "%wheel ALL=(ALL) NOPASSWD: ALL",
   }
   sudo::conf {'rbackup':
-    content => "Defaults:rbackup !requiretty
+    content => "  Defaults:rbackup !requiretty
 rbackup ALL = NOPASSWD: /usr/bin/rsync"}
 }
 
