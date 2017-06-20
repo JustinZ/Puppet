74edc7415ba8d2fb986dde48e30ed70eeeed50b6
Justin Zhang <justin.zhang@digitalalchemy.asia>
2017-06-20 14:49:22 +1000

    add dir

--- site/profile/manifests/nginx.pp
+++ site/profile/manifests/nginx.pp
@@ -4,6 +4,9 @@ nginx::resource::server { "${fqdn}":
   www_root => "/var/www/${fqdn}",
 }
 file {"/var/www/${fqdn}":
+ensure  => directory,  
+}
+file {"/var/www/${fqdn}/index.html":
 ensure  => present,  
 content => "Hello World",  
 }
