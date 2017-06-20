3cc6ef2233a3e56f3972dbe421cade1faba5b152
Justin Zhang <justin.zhang@digitalalchemy.asia>
2017-06-20 15:17:22 +1000

    dir tree

--- site/profile/manifests/nginx.pp
+++ site/profile/manifests/nginx.pp
@@ -3,6 +3,9 @@ class { 'nginx': }
 nginx::resource::server { "${fqdn}":
   www_root => "/var/www/${fqdn}",
 }
+file {"/var/www":
+ensure  => directory,  
+}
 file {"/var/www/${fqdn}":
 ensure  => directory,  
 }
