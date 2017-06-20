e99186a01bf4f0c0ded1011ba64216bc1abbdd4f
Justin Zhang <justin.zhang@digitalalchemy.asia>
2017-06-20 13:50:11 +1000

    index fix

--- site/profile/manifests/nginx.pp
+++ site/profile/manifests/nginx.pp
@@ -3,8 +3,8 @@ class { 'nginx': }
 nginx::resource::server { "${fqdn}":
   www_root => "/var/www/${fqdn}",
 }
-file {"/var/www/${fqdn}/index.html":
-ensure  => present  
-content => "Hello World";  
+file {"/var/www/${fqdn}":
+ensure  => present,  
+content => "Hello World",  
 }
 }
