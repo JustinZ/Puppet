6f3946c6e99bc34809d4d6e61d56500b59645827
Justin Zhang <justin.zhang@digitalalchemy.asia>
2017-06-22 19:06:27 +1000

    update passwd

--- site/profile/manifests/mysql.pp
+++ site/profile/manifests/mysql.pp
@@ -2,10 +2,10 @@ class profile::mysql {
 include '::mysql::server'
 mysql::db { 'ENC_hosts':
   user     => 'justin',
-  password => '$1$xx$3CfaGhl7ktDkSY.ENv0iD.',
+  password => '$1$RpKd1sbB$GVNGgw.CEEybjHATCU/lK1',
   host     => 'localhost',
   grant    => ['ALL'],
-  sql      => '/home/justin/puppet_classification_db.sql',
-  import_timeout => 900,
+  #sql      => '/home/justin/puppet_classification_db.sql',
+  #import_timeout => 900,
 }
 }
