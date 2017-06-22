6787e2509e8630f48f87f09bd6d3b6ee3d7e7828
Justin Zhang <justin.zhang@digitalalchemy.asia>
2017-06-22 12:21:12 +1000

    add db ENC_hosts

--- site/profile/manifests/mysql.pp
+++ site/profile/manifests/mysql.pp
@@ -1,4 +1,9 @@
 class profile::mysql {
 include '::mysql::server'
+mysql::db { 'ENC_hosts':
+  user     => 'justin',
+  password => '$1$Fid45cI9$EoV5Nyft0QhLdZSqpdJSo1',
+  host     => 'localhost',
+  grant    => ['ALL'],
 }
-
+}
