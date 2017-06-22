ca0f5bd5f4c5dc19d26449f0d8d55e2d4dd49af6
Justin Zhang <justin.zhang@digitalalchemy.asia>
2017-06-22 16:26:56 +1000

    update sql

--- site/profile/manifests/mysql.pp
+++ site/profile/manifests/mysql.pp
@@ -5,7 +5,7 @@ mysql::db { 'ENC_hosts':
   password => '$1$xx$3CfaGhl7ktDkSY.ENv0iD.',
   host     => 'localhost',
   grant    => ['ALL'],
-  sql      => /home/justin/puppet_classification_db.sql,
+  sql      => '/home/justin/puppet_classification_db.sql',
   import_timeout => 900,
 }
 }
