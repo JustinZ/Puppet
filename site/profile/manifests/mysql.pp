f418f47fbdaa557d583af560f7cb37a6bfa1b87c
Justin Zhang <justin.zhang@digitalalchemy.asia>
2017-06-22 16:22:55 +1000

    add sql file

--- site/profile/manifests/mysql.pp
+++ site/profile/manifests/mysql.pp
@@ -2,8 +2,10 @@ class profile::mysql {
 include '::mysql::server'
 mysql::db { 'ENC_hosts':
   user     => 'justin',
-  password => '$1$Fid45cI9$EoV5Nyft0QhLdZSqpdJSo1',
+  password => '$1$xx$3CfaGhl7ktDkSY.ENv0iD.',
   host     => 'localhost',
   grant    => ['ALL'],
+  sql      => /home/justin/puppet_classification_db.sql,
+  import_timeout => 900,
 }
 }
