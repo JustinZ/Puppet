1c0477a7d408e200c8f706be7bb538e09923493a
Justin Zhang <justin.zhang@digitalalchemy.asia>
2017-10-19 18:41:18 +1100

    change

--- site/profile/manifests/nginx.pp
+++ site/profile/manifests/nginx.pp
@@ -16,6 +16,8 @@ content => "Hello World",
 }
 fooacl::conf { '${fqdn}':
   target      => '/var/www/${fqdn}',
-  permissions => '$fooacl_permissons',
+  permissions => [
+  $fooacl_permissons,
+  ]
 }
 }
